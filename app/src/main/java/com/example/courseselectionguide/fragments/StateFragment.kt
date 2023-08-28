64c5d4df7e2c2f6174b25812629cec32b20529b8
Fatemeh <fatemehvarmaghani.main@gmail.com>
2023-08-28 14:55:26 -0200

    removed rtl direction in selection and recommendation fragments' toolbar, toolbar and appbar and linear to ui

--- app/src/main/java/com/example/courseselectionguide/fragments/StateFragment.kt
+++ app/src/main/java/com/example/courseselectionguide/fragments/StateFragment.kt
@@ -5,21 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.courseselectionguide.R
 import com.example.courseselectionguide.databinding.FragmentStateBinding
 
 class StateFragment : Fragment() {
-
     private lateinit var binding: FragmentStateBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentStateBinding.inflate(inflater)
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //pie chart
+
     }
 
 }
