ffe737e0e74c5ba5b593311ab156c433733f1494
Fatemeh <fatemehvarmaghani.main@gmail.com>
2023-08-30 13:31:02 -0200

    manifest(start activity), pie chart lib & repo, kotlin update, add pie chart, icons & ui customization in kotlin code, new color: gray, changing toolbars to androidx & font & theme

--- app/src/main/java/com/example/courseselectionguide/fragments/StateFragment.kt
+++ app/src/main/java/com/example/courseselectionguide/fragments/StateFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.example.courseselectionguide.R
 import com.example.courseselectionguide.databinding.FragmentStateBinding
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
 
 class StateFragment : Fragment() {
     private lateinit var binding: FragmentStateBinding
+    private lateinit var pieChart: PieChart
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,7 +28,30 @@ class StateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //pie chart
-
+        pieChart = binding.pieChartState
+        val pieEntries = arrayListOf(
+            PieEntry(25f),
+            PieEntry(5f),
+            PieEntry(70f)
+        )
+        val pieDataSet = PieDataSet(pieEntries, "")
+        val colors = arrayListOf(
+            ContextCompat.getColor(requireContext(), R.color.cyan),
+            ContextCompat.getColor(requireContext(), R.color.orange),
+            ContextCompat.getColor(requireContext(), R.color.light_gray)
+        )
+        pieDataSet.colors = colors
+        pieChart.data = PieData(pieDataSet)
+        pieChart.description.isEnabled = false
+        pieChart.setUsePercentValues(false)
+        pieChart.legend.isEnabled = false
+        pieDataSet.valueFormatter = object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return ""
+            }
+        }
+        pieChart.animateY(1000)
+        pieChart.invalidate()
     }
 
 }
