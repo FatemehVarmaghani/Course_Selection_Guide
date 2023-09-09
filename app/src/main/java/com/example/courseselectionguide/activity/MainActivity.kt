3ba20515fbef733375898fb2eee9dab5a9b68c69
Fatemeh <fatemehvarmaghani.main@gmail.com>
2023-09-09 16:52:12 -0200

    an instance of data in main activity

--- app/src/main/java/com/example/courseselectionguide/activity/MainActivity.kt
+++ app/src/main/java/com/example/courseselectionguide/activity/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.courseselectionguide.R
+import com.example.courseselectionguide.data.CorequisitesList
+import com.example.courseselectionguide.data.Lessons
+import com.example.courseselectionguide.data.PrerequisitesList
 import com.example.courseselectionguide.databinding.ActivityMainBinding
 import com.example.courseselectionguide.fragments.RecommendationFragment
 import com.example.courseselectionguide.fragments.SelectedLessonsFragment
 import com.example.courseselectionguide.fragments.StateFragment
 
 lateinit var sharedPref: SharedPreferences
+lateinit var dataList: ArrayList<Lessons>
 
 class MainActivity : AppCompatActivity() {
 
@@ -58,6 +62,94 @@ class MainActivity : AppCompatActivity() {
 
         }
         binding.mainBottomNavigation.setOnItemReselectedListener { }
+
+        //data from database
+        dataList = arrayListOf(
+            Lessons(
+                lessonName = "ریاضی عمومی 1",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "فیزیک 1",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "فارسی عمومی",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 2,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "زبان عمومی",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 2,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "مبانی کامپیوتر و برنامه سازی",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 4,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "اندیش اسلامی 1",
+                theoreticalUnitNumber = 2f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 2,
+                recommendedSemester = 1,
+                lessonOrientationId = 1
+            ),
+            Lessons(
+                lessonName = "تاریخ فرهنگ و تمدن اسلام و ایران",
+                theoreticalUnitNumber = 2f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 1,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "فیزیک 2",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 2,
+                listOfPrerequisites = PrerequisitesList(arrayListOf(2))
+            ),
+            Lessons(
+                lessonName = "ریاضی عمومی 2",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 2,
+                listOfPrerequisites = PrerequisitesList(arrayListOf(1))
+            ),
+            Lessons(
+                lessonName = "ریاضیات گسسته",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 4,
+                recommendedSemester = 2,
+                listOfCorequisites = CorequisitesList(arrayListOf(1, 5))
+            )
+        )
     }
 
     // to open State Fragment when the app is opened
