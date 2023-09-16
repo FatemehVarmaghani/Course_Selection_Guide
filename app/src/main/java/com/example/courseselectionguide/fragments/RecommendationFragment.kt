62443d08b9f0aa3b9f788f0a6c4c363d83babae6
Fatemeh <fatemehvarmaghani.main@gmail.com>
2023-09-16 17:10:05 -0200

    recycler and list variable for selected and recommended lists

--- app/src/main/java/com/example/courseselectionguide/fragments/RecommendationFragment.kt
+++ app/src/main/java/com/example/courseselectionguide/fragments/RecommendationFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.courseselectionguide.R
 import com.example.courseselectionguide.adapter.AdapterLessons
-import com.example.courseselectionguide.data.SelectedLessons
+import com.example.courseselectionguide.data.CorequisitesList
+import com.example.courseselectionguide.data.Lessons
+import com.example.courseselectionguide.data.PrerequisitesList
+import com.example.courseselectionguide.databinding.DialogLessonDetailBinding
 import com.example.courseselectionguide.databinding.FragmentRecommendationBinding
 
-class RecommendationFragment : Fragment() {
+class RecommendationFragment : Fragment(), AdapterLessons.ItemEvents {
 
     private lateinit var binding: FragmentRecommendationBinding
 
@@ -45,10 +50,147 @@ class RecommendationFragment : Fragment() {
             }
         }
 
-//        //show lessons on recyclerView
-//        val lessonsAdapter = AdapterLessons(data)
-//        binding.recyclerRecommendation.adapter = lessonsAdapter
-//        binding.recyclerRecommendation.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        //list of selected lessons
+        val dataList = arrayListOf(
+            Lessons(
+                lessonName = "ریاضی عمومی 1",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "فیزیک 1",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "فارسی عمومی",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 2,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "زبان عمومی",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 2,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "مبانی کامپیوتر و برنامه سازی",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 4,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "اندیش اسلامی 1",
+                theoreticalUnitNumber = 2f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 1,
+                recommendedSemester = 1,
+                lessonOrientationId = 1
+            ),
+            Lessons(
+                lessonName = "تاریخ فرهنگ و تمدن اسلام و ایران",
+                theoreticalUnitNumber = 2f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 2,
+                recommendedSemester = 1
+            ),
+            Lessons(
+                lessonName = "فیزیک 2",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 2,
+                listOfPrerequisites = PrerequisitesList(arrayListOf(2))
+            ),
+            Lessons(
+                lessonName = "ریاضی عمومی 2",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 3,
+                recommendedSemester = 2,
+                listOfPrerequisites = PrerequisitesList(arrayListOf(1))
+            ),
+            Lessons(
+                lessonName = "ریاضیات گسسته",
+                theoreticalUnitNumber = 3f,
+                practicalUnitNumber = 0f,
+                unitType = true,
+                lessonTypeId = 4,
+                recommendedSemester = 2,
+                listOfCorequisites = CorequisitesList(arrayListOf(1, 5))
+            )
+        )
+
+        //show lessons on recyclerView
+        val lessonsAdapter = AdapterLessons(requireContext(), dataList, this)
+        binding.recyclerRecommendation.adapter = lessonsAdapter
+        binding.recyclerRecommendation.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
     }
+
+    override fun onItemClicked(lesson: Lessons) {
+        //lesson details
+        val lessonInfoDialogBinding = DialogLessonDetailBinding.inflate(layoutInflater)
+        lessonInfoDialogBinding.infoDialogLessonName.text = lesson.lessonName
+        //you can change it later by the use of Room:
+        lessonInfoDialogBinding.infoDialogLessonType.text = when (lesson.lessonTypeId) {
+            1 -> "عمومی اسلامی"
+            2 -> "عمومی"
+            3 -> "پایه"
+            4 -> "اصلی"
+            5 -> "تخصصی"
+            6 -> "تمرکز تخصصی"
+            7 -> "اختیاری"
+            else -> ""
+        }
+        // for lessons which have both theo and prac units, show float, for others show int:
+        if (lesson.unitType == null) {
+            lessonInfoDialogBinding.infoDialogTheoreticalUnits.text =
+                lesson.theoreticalUnitNumber.toString()
+            lessonInfoDialogBinding.infoDialogPracticalUnits.text =
+                lesson.practicalUnitNumber.toString()
+        } else {
+            lessonInfoDialogBinding.infoDialogTheoreticalUnits.text =
+                lesson.theoreticalUnitNumber.toInt().toString()
+            lessonInfoDialogBinding.infoDialogPracticalUnits.text =
+                lesson.practicalUnitNumber.toInt().toString()
+        }
+        //checking pre and co requisites:
+        if (lesson.listOfPrerequisites == null) {
+            lessonInfoDialogBinding.infoDialogPrerequisites.text = "ندارد"
+        } else {
+            lessonInfoDialogBinding.infoDialogPrerequisites.text =
+                lesson.listOfPrerequisites.toString() //extract from database (don't forget forEach)
+        }
+        if (lesson.listOfCorequisites == null) {
+            lessonInfoDialogBinding.infoDialogCorequisites.text = "ندارد"
+        } else {
+            lessonInfoDialogBinding.infoDialogCorequisites.text =
+                lesson.listOfCorequisites.toString() // extract from database
+        }
+
+        //create & show dialog
+        val lessonInfoDialog = AlertDialog.Builder(requireContext())
+        lessonInfoDialog.setView(lessonInfoDialogBinding.root)
+        lessonInfoDialog.create().show()
+    }
+
+    override fun onOptionsIconClicked(item: View, popupMenu: PopupMenu) {}
 
 }
